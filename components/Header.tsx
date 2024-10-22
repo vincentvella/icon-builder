46e4371ef8d20fc0be40d5c89b241dcf45e66c97
Vince Vella <vince@Vinces-M3-MacBook-Pro.local>
2024-10-21 22:52:35 -0400

    feat: rebrand and add github link

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,9 @@
 import { useTheme } from "@/hooks/theme/ThemeContext";
 import { A, Header as HeaderElement } from "@expo/html-elements";
+import Ionicons from "@expo/vector-icons/Ionicons";
 import DarkModeSwitch from "expo-dark-mode-switch";
 import { Text, View } from "react-native";
+import colors from "tailwindcss/colors";
 
 export function Header() {
   const { colorScheme, toggleColorScheme } = useTheme();
@@ -10,10 +12,20 @@ export function Header() {
       <View className="flex-row items-center p-4">
         <A href="/">
           <Text className="text-black dark:text-white text-2xl my-4">
-            Icon Builder
+            App Icon Builder
           </Text>
         </A>
         <View className="flex-1" />
+        <A
+          className="align-center mr-2 pt-0.5"
+          href="https://github.com/vincentvella/icon-builder"
+        >
+          <Ionicons
+            name="logo-github"
+            size={35}
+            color={colorScheme === "dark" ? colors.white : colors.black}
+          />
+        </A>
         <DarkModeSwitch
           value={colorScheme === "dark"}
           onChange={() => {
